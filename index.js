0fde76866482d116c3f541179dc654509ade1ffa
Edwin shikuku <shikuku.ochieng@gmail.com>
2022-08-22 06:17:59 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue>42){
+        return someValue-42;
+    }else{
+        return 42-someValue;
+    }
+}
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(264*(destination-start));
+}
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start, destination)<400){
+        return 0;
+    }else if (distanceTravelledInFeet(start, destination)<=2000){
+        return (distanceTravelledInFeet(start, destination)-400)/50;
+    }else if (distanceTravelledInFeet(start, destination)>2500){
+        return "cannot travel that far";
+    }else{
+        return 25;
+    }
+}
